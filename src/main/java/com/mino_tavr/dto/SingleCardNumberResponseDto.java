171d967a16c2f725e809f47da74d8fa57870b363
Павел Климов <klimovpavel62@gmail.com>
2023-03-03 12:18:17 +0300

    Init

--- src/main/java/com/mino_tavr/dto/SingleCardNumberResponseDto.java
+++ src/main/java/com/mino_tavr/dto/SingleCardNumberResponseDto.java
@@ -15,7 +15,7 @@ public class SingleCardNumberResponseDto {
     Integer id_card_number;
     String number;
     BufferedImage image;
-    String device_type;
+    int device_type;
     int interaction_id;
     int reason_id;
     List<DescriptionResponseDto> description;
